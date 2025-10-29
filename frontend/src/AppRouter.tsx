91d37bcaa22d678b5046f034fcfe71a25cbbb976
Tharushi Dananjana <mgtharushidananjana@gmail.com>
2025-10-29 16:31:11 +0530

    add

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-  import { BrowserRouter, Routes, Route } from "react-router-dom";
-  import { App } from "./App";
 
-  export function AppRouter() {
-    return (
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-          </Routes>
-      </BrowserRouter>
-    );
-  }
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { App } from "./App";
+
+export function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
